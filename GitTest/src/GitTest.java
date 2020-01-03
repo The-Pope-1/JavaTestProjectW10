05cc7b26462674b45dfde6ce496c796c51448f19
PC <PC@192.168.178.63>
2020-01-03 21:13:04 +0100

    ersetzt

--- GitTest/src/GitTest.java
+++ GitTest/src/GitTest.java
@@ -4,20 +4,16 @@ public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("Git-Test-Projekt Ausgabe ...");
 		
-		aWin8Func();		
+		aWin8Func();
+		aWin8Func2();
 	}
 
 	public static void aWin8Func() {
 		System.out.println("Ich bin eine Ausgabe, die in Win8 erstellt wurde.");
 	}
-<<<<<<< HEAD
 	
-	public static void ttt() {
-		System.out.println("ttt");
-=======
 	public static void aWin8Func2() {
 		System.out.println("Ich bin eine Ausgabe, die in Win8 erstellt wurde.");
->>>>>>> branch 'master' of https://github.com/The-Pope-1/JavaTestProjectW10.git
 	}
 
 }
