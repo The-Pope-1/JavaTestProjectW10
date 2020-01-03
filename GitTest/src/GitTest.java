70817c48b7e66588b55706cd90ed81150137d401
PC <PC@192.168.178.63>
2020-01-03 20:18:04 +0100

    Win8 Test Ausgabe

--- GitTest/src/GitTest.java
+++ GitTest/src/GitTest.java
@@ -3,7 +3,14 @@ public class GitTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Git-Test-Projekt Ausgabe ...");
+		System.out.println("Git-Test-Projekt Ausgabe ...");
+		
+		aWin8Func();
+		
+	}
+	
+	public static void aWin8Func() {
+		System.out.println("Ich bin eine Ausgabe, die in Win8 erstellt wurde.");
 	}
 
 }
